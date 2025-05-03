a02449e51a46f4a287f250e5d64b07e17305907d
Lior Weintraub <LiorW@pliops.com>
2025-05-03 21:15:19 +0300

    Implement a function that takes a `WineRegions` value as input and returns a string representation of the region's popularity

--- examples/12-enum-types/enum-types/src/main.rs
+++ examples/12-enum-types/enum-types/src/main.rs
@@ -6,6 +6,22 @@ enum WineRegions {
     Tuscany,
     Rioja,
     NapaValley,
+    SeaOfGalilee,
+    GolanHeights,
+}
+impl WineRegions {
+    fn copy(&self) -> WineRegions {
+        match self {
+            WineRegions::Bordeaux => WineRegions::Bordeaux,
+            WineRegions::Burgundy => WineRegions::Burgundy,
+            WineRegions::Champagne => WineRegions::Champagne,
+            WineRegions::Tuscany => WineRegions::Tuscany,
+            WineRegions::Rioja => WineRegions::Rioja,
+            WineRegions::NapaValley => WineRegions::NapaValley,
+            WineRegions::SeaOfGalilee => WineRegions::SeaOfGalilee,
+            WineRegions::GolanHeights => WineRegions::GolanHeights,
+        }
+    }
 }
 
 struct Wine {
@@ -19,7 +35,18 @@ fn supported_regions(w: WineRegions) {
         _ => println!("{:?} is not supported!", w),
     }
 }
-
+fn region_popularity(w: WineRegions) {
+    match w {
+        WineRegions::Bordeaux => println!("Bordeaux is very popular!"),
+        WineRegions::Burgundy => println!("Burgundy is also popular!"),
+        WineRegions::Champagne => println!("Champagne is a classic!"),
+        WineRegions::Tuscany => println!("Tuscany is known for its Chianti!"),
+        WineRegions::Rioja => println!("Rioja is famous for its Tempranillo!"),
+        WineRegions::NapaValley => println!("Napa Valley is known for its Cabernet Sauvignon!"),
+        WineRegions::SeaOfGalilee => println!("Sea of Galilee is known for its unique wines!"),
+        WineRegions::GolanHeights => println!("Golan Heights is known for its high-quality wines!"),
+    }
+}
 fn main() {
     let wine1 = Wine {
         name: String::from("Chateau Margaux"),
@@ -31,8 +58,19 @@ fn main() {
         region: WineRegions::Tuscany,
     };
 
-    // println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
-    // println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
-    supported_regions(wine1.region);
+    let wine3 = Wine {
+        name: String::from("Carmel"),
+        region: WineRegions::GolanHeights,
+    };
+
+    println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
+    println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
+    println!("Wine 3: {} from {:?}", wine3.name, wine3.region);
+    // clone the wine1 object
+    let wine1_region = wine1.region.copy();
+    supported_regions(wine1_region);
     supported_regions(WineRegions::Rioja);
+    region_popularity(wine1.region);
+    region_popularity(wine2.region);
+    region_popularity(wine3.region);
 }
