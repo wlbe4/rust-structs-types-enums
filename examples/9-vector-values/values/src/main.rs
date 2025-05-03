f0a7a52fbe4a9f0d1eb146cff0a530989fb4453c
Lior Weintraub <LiorW@pliops.com>
2025-05-03 13:36:49 +0300

    Add a function that takes a vector as input and returns the sum of all its elements

--- examples/9-vector-values/values/src/main.rs
+++ examples/9-vector-values/values/src/main.rs
@@ -10,21 +10,32 @@ fn get_item(index: usize) {
     println!("The value at index {} is {:?}", index, value);
 }
 
+fn sum_vec(vec: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in vec {
+        sum += i;
+    }
+    sum
+}
+
 fn main() {
     let vec = vec![1, 2, 3, 4, 5];
-    get_item(3);
+    // let vec: Vec<i32> = vec![];
+    // get_item(3);
 
     // Retrieve a value at a specific index
-    let third_value = vec[2];
+    // let third_value = vec[2];
     //println!("The third value in the vector is: {}", third_value);
 
     // Retrieve the last value
-    let last_value = vec.last().unwrap();
+    // let last_value = vec.last().unwrap();
     //println!("The last value in the vector is: {}", last_value);
 
     // Retrieve the first value using pattern matching
-    // match vec.first() {
-    //     Some(first_value) => println!("The first value in the vector is: {}", first_value),
-    //     None => println!("The vector is empty!"),
-    // }
+    match vec.first() {
+        Some(first_value) => println!("The first value in the vector is: {}", first_value),
+        None => println!("The vector is empty!"),
+    }
+    let sump = sum_vec(vec);
+    println!("The sum of the vector is: {}", sump);
 }
