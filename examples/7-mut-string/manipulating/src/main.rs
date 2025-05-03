26d888ee32d49558361644e02301ed2d595bafc2
Lior Weintraub <LiorW@pliops.com>
2025-05-03 13:19:38 +0300

    count the number of occurrences of each vowel

--- examples/7-mut-string/manipulating/src/main.rs
+++ examples/7-mut-string/manipulating/src/main.rs
@@ -4,10 +4,26 @@ fn main() {
     //println!("{}", &sentence[0..=4]);
 
     // concatenate using format!
-    let description = format!("Title: Quick story\n{}", sentence);
+    // let description = format!("Title: Quick story\n{}", sentence);
     //println!("{}", description);
 
     // iterate over the characters in the sentence
+    let mut a = 0;
+    let mut e = 0;
+    let mut i = 0;
+    let mut o = 0;
+    let mut u = 0;
+    for c in sentence.chars() {
+        match c {
+            'a' => a += 1,
+            'e' => e += 1,
+            'i' => i += 1,
+            'o' => o += 1,
+            'u' => u += 1,
+            _ => continue,
+        }
+    }
+    println!("a: {}, e: {}, i: {}, o: {}, u: {}", a, e, i, o, u);
     // for c in sentence.chars() {
     //     match c {
     //         'a' | 'e' | 'i' | 'o' | 'u' => println!("Got a vowel!"),
@@ -17,9 +33,9 @@ fn main() {
 
     // Split and collect into a vector
     //let words: Vec<&str> = sentence.split_whitespace().collect();
-    let words = sentence.split(' ').collect::<Vec<&str>>();
+    //let words = sentence.split(' ').collect::<Vec<&str>>();
     //println!("{:?}", words);
 
-    let reversed = sentence.chars().rev().collect::<String>();
-    println!("{}", reversed);
+    //let reversed = sentence.chars().rev().collect::<String>();
+    //println!("{}", reversed);
 }
